6e7257b85018d832526df604d402648335fbe086
Aashrya Shrivastava <aashryashrivastava@gmail.com>
2023-02-23 02:20:02 +0530

    added components

---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import react from "react";
+
+function Note(){
+    return <div className="note">
+        <h1>this is title</h1>
+        <p>this is for content</p>
+    </div>
+}
+
+export default Note;
