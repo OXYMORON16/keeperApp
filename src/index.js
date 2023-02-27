2c09192e0363e32bc3796af6e84a4a414ce15a0c
Aashrya Shrivastava <aashryashrivastava@gmail.com>
2023-02-27 15:37:43 +0530

    added more notes components using map function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 import React from "react";
-import reactDom from "react-dom";
+import ReactDOM from "react-dom";
 import App from "./components/App";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Note from "./components/Note";
 
-reactDom.render(
-    <div>
-    <Header />
-    <Note />
-    <Footer />
-    </div>
-,
- document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
+
